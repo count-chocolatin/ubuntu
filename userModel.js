6c36fa417397b01188c9ac6393223b3c3727dc74
manuel <sorrinok@gmail.com>
2021-03-20 15:05:46 -0500

    ubuntugit

---
+++ userModel.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+   name: { 
+      type: String,
+      required: true
+   },
+   email: {
+     type: String,
+     required: true,
+     unique: true
+   },
+   password: {
+     type: String,
+     required: true
+   },
+   role: {
+    type: String,
+    default:'user'
+   },
+   root: {
+    type: Boolean,
+    default: false
+   },
+   avatar: {
+    type: String,
+    default: 'https://res.cloudinary.com/dfh7dqq82/image/upload/v1615407960/sample.jpg'
+   }
+}, {
+         timestamps: truekñk
+})
+
+let Dataset = mongoose.model.user || mongoose.model('user', userSchema)
+export default Datasetlñkñkññ
+pjñjkñjk
